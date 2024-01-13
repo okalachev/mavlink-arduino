f96fe314e0508bc40770b8a974b10c7238d4a837
Oleg Kalachev <okalachev@gmail.com>
2024-01-13 15:10:39 +0300

    Add headers, minor library properties fix

--- MAVLink_development.h
+++ MAVLink_development.h
@@ -1,1 +1,6 @@
+// MAVLink-Arduino
+// Author: Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/mavlink-arduino
+// Licence: MIT
+
 #include "mavlink/development/mavlink.h"
